f9b4fca2e5d17a536819101cf4c86fc2356a4873
Aleck David Holly <aleckdavidholly@gmail.com>
2022-12-02 23:59:40 -0500

    Initial Commit

---
+++ NXT LVL/NXT_LVLApp.swift
@@ -0,0 +1,17 @@
+//
+//  NXT_LVLApp.swift
+//  NXT LVL
+//
+//  Created by Aleck Holly on 2022-12-02.
+//
+
+import SwiftUI
+
+@main
+struct NXT_LVLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
